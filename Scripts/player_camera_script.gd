827f0179471c10d4b7f4e6ae6d45ba6c16e65ffe
CannedFoody <gamingman72069@gmail.com>
2026-03-15 22:58:51 +0200

    Added some placeholder textures to the test_level as well as some walls. Added a pause menu and an info menu which you can toggle with F1.

--- Scripts/player_camera_script.gd
+++ Scripts/player_camera_script.gd
@@ -17,20 +17,21 @@ func _ready() -> void:
 	player_controller = get_parent()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if (event is InputEventMouseMotion) and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		mouse_input.x += -event.screen_relative.x * mouse_sensitivity
 		mouse_input.y += -event.screen_relative.y * mouse_sensitivity
 
 func _process(_delta: float) -> void:
-	input_rotation.x = clampf(input_rotation.x + mouse_input.y, deg_to_rad(-90), deg_to_rad(85))
-	input_rotation.y += mouse_input.x
-	
-	# rotate camera controller (up/down)
-	player_controller.camera_pivot_anchor.transform.basis = Basis.from_euler(Vector3(input_rotation.x, 0.0, 0.0))
-	
-	# rotate player (left/right)
-	player_controller.global_transform.basis = Basis.from_euler(Vector3(0.0, input_rotation.y, 0.0))
-	
-	global_transform = player_controller.camera_pivot_anchor.get_global_transform_interpolated()
-	
-	mouse_input = Vector2.ZERO
+	if (Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
+		input_rotation.x = clampf(input_rotation.x + mouse_input.y, deg_to_rad(-90), deg_to_rad(85))
+		input_rotation.y += mouse_input.x
+		
+		# rotate camera controller (up/down)
+		player_controller.camera_pivot_anchor.transform.basis = Basis.from_euler(Vector3(input_rotation.x, 0.0, 0.0))
+		
+		# rotate player (left/right)
+		player_controller.global_transform.basis = Basis.from_euler(Vector3(0.0, input_rotation.y, 0.0))
+		
+		global_transform = player_controller.camera_pivot_anchor.get_global_transform_interpolated()
+		
+		mouse_input = Vector2.ZERO
--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -4,12 +4,24 @@
 class_name PlayerController
 extends CharacterBody3D
 
+@onready var pause_menu: Control = $"Pause Menu/Pause_Menu"
+
+# Labels for information
+@onready var x_pos: Label = $PanelContainer/VBoxContainer/x_pos
+@onready var y_pos: Label = $PanelContainer/VBoxContainer/y_pos
+@onready var z_pos: Label = $PanelContainer/VBoxContainer/z_pos
+@onready var velocity_x: Label = $PanelContainer/VBoxContainer/velocity_x
+@onready var velocity_y: Label = $PanelContainer/VBoxContainer/velocity_y
+@onready var panel_container: PanelContainer = $PanelContainer
+
+var paused = false
+var info_hidden = true
+
+# Camera vars
 @onready var camera_3d: Camera3D = $"Camera-Pivot/Camera3D"
 @onready var camera_pivot_anchor: Marker3D = $"Camera-Pivot-Anchor"
 
-var interact_obj: Node3D = null
-var holding_obj = false
-
+# Player movement vars
 @export var player_speed = 5.0
 @export var player_jump_velocity = 4.5
 @export var player_sprint_str = 5.0
@@ -18,34 +30,69 @@ var holding_obj = false
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().quit()
-		
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * _delta
-		
-	# Sprint functionality
-	if Input.is_action_just_pressed("Sprint"):
-		player_speed = player_speed + player_sprint_str
-	if Input.is_action_just_released("Sprint"):
-		player_speed = player_speed - player_sprint_str
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = player_jump_velocity
-		
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction = (camera_3d.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	direction.y = 0
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("Pause")):
+		pause_menu_functionality()
 	
-	if direction:
-		velocity.x = direction.x * player_speed
-		velocity.z = direction.z * player_speed
+#	Debug information panel toggle and information toggle.
+	
+	x_pos.text = "X | " + str(global_position.x)
+	y_pos.text = "Y | " + str(global_position.y)
+	z_pos.text = "Z | " + str(global_position.z)
+	velocity_x.text = "Velocity X | " + str(velocity.x)
+	velocity_y.text = "Velocity Y | " + str(velocity.y)
+	
+	if(info_hidden):
+		panel_container.hide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, player_speed)
-		velocity.z = move_toward(velocity.z, 0, player_speed)
-	move_and_slide()
+		panel_container.show()
+		
+	if(Input.is_action_just_pressed("Info_Toggle")):
+		info_hidden = !info_hidden
+		
+func _physics_process(_delta: float) -> void:
+	if (Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * _delta
+			
+		# Sprint functionality
+		if Input.is_action_just_pressed("Sprint"):
+			player_speed = player_speed + player_sprint_str
+		if Input.is_action_just_released("Sprint"):
+			player_speed = player_speed - player_sprint_str
 
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = player_jump_velocity
+			
+		var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+		var direction = (camera_3d.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		direction.y = 0
+		
+		if direction:
+			velocity.x = direction.x * player_speed
+			velocity.z = direction.z * player_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, player_speed)
+			velocity.z = move_toward(velocity.z, 0, player_speed)
+		move_and_slide()
+		
+func pause_menu_functionality():
+	if(paused):
+		pause_menu.hide()
+		Engine.time_scale = 1
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	paused = !paused
+
+
+func _on_resume_pressed() -> void:
+	pause_menu_functionality()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
